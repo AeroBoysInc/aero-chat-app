4f1f04d26dce7fd0d4faa8fa34f1a44622712988
DAndovski <dejancarter23@gmail.com>
2026-03-24 11:26:36 +0100

    feat: add playingGames map to presenceStore

--- src/store/presenceStore.ts
+++ src/store/presenceStore.ts
@@ -3,13 +3,17 @@ import { create } from 'zustand';
 interface PresenceState {
   onlineIds: Set<string>;
   presenceReady: boolean;
+  playingGames: Map<string, string>;
   setOnlineIds: (ids: Set<string>) => void;
   setPresenceReady: (ready: boolean) => void;
+  setPlayingGames: (games: Map<string, string>) => void;
 }
 
 export const usePresenceStore = create<PresenceState>((set) => ({
-  onlineIds: new Set<string>(),
+  onlineIds:     new Set<string>(),
   presenceReady: false,
-  setOnlineIds: (ids) => set({ onlineIds: ids }),
+  playingGames:  new Map<string, string>(),
+  setOnlineIds:     (ids)   => set({ onlineIds: ids }),
   setPresenceReady: (ready) => set({ presenceReady: ready }),
+  setPlayingGames:  (games) => set({ playingGames: games }),
 }));
